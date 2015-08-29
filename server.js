e9719f56660056e96ab43748a3ed79599cf5dce0
Yuwei Xu <yuwei_xu@outlook.com>
2015-08-29 09:40:12 -0700

    have basic setup for mentoring requests

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var parseOptions = {
 
 firebaseRef.child("/mobile/updates").on("child_added", function(snapshot) {
     var update = snapshot.val();
-    var parseRequestForm = {
+    var updateForm = {
         where: { 
             updates_enabled: true
         }, 
@@ -29,10 +29,35 @@ firebaseRef.child("/mobile/updates").on("child_added", function(snapshot) {
         }
     };
 
-    parseOptions['body'] = parseRequestForm;
+    parseOptions['body'] = updateForm;
     request(parseOptions, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log("REQUEST WORKED");
+        }
+        else {
+            console.log(body);
+        }
+    });
+
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+
+firebaseRef.child("/mobile/mentoring/requests").on("child_added", function(snapshot) {
+    var mentoringRequest = snapshot.val();
+    var mentoringRequestForm = {
+        where: { 
+            mentoring_requests_enabled: true
+        }, 
+        data: { 
+            alert: mentoringRequest['hacker'].name + " needs a mentor!"
+        }
+    };
+
+    parseOptions['body'] = mentoringRequestForm;
+    request(parseOptions, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log("MENTORING REQUEST WORKED");
         }
         else {
             console.log(body);
