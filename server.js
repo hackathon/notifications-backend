b694b4cec28da1dd68878106be71121a1b7d3dcd
Yuwei Xu <yuwei_xu@outlook.com>
2015-08-28 23:04:27 -0700

    restructure http request

--- server.js
+++ server.js
@@ -1,38 +1,43 @@
 var Firebase = require("firebase");
 var request = require("request");
+var config = require("./config");
 
 var firebaseRef = new Firebase("https://hackthenorth.firebaseio.com/");
-var parseRequestUrl = 'http://www.api.parse.com/1/push';
 
-var parseOptions = {
-  url: 'https://api.github.com/repos/request/request',
-  headers: {
-    'X-Parse-Application-Id': 'asdfsa',
-    'X-Parse-REST-API-Key': 'asdfasd',
-    'Content-Type': 'application/json'
-  }
+var parseRequestForm = {
+    where: {
+        updates_enabled: true
+    },
+    data: {
+        title:' update.name',  // Android-only feature
+        alert: 'update.description'
+    }
 };
 
+var parseOptions = {
+    uri: 'https://api.parse.com/1/push',
+    method: 'POST',
+    form: parseRequestForm,
+    headers: {
+        'X-Parse-Application-Id': PARSE_APPLICATION_ID,
+        'X-Parse-REST-API-Key': PARSE_REST_API_KEY,
+        'Content-Type': 'application/json',
+    }
+};
 
 firebaseRef.child("/mobile/updates").on("child_added", function(snapshot) {
     var update = snapshot.val();
-    var parseRequestForm = {
-        where: {
-            mentoring_requests_enabled: true
-        }
-        data: {
-            title: update.title,  // Android-only feature
-            alert: update.body
-        }
-    }
 
-    request.post({url: parseRequestUrl, form: parseRequestForm, options: parseOptions}, 
-        function (error, response, body) {
+    request(parseOptions, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log(body) // Show the HTML for the Google homepage.
+            console.log("REQUEST WORKED");
         }
-    })
+        else {
+            console.log(body);
+            console.log(response.statusCode);
+        }
+    });
 
 }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
-})
+});
